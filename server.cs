b893e029fe76f6fb9bc3f11c7046f2090aab3fd8
PrivateCaller <waffleman2000@protonmail.com>
2022-10-14 00:57:12 -0500

    Flavor Message System Prototype.

--- server.cs
+++ server.cs
@@ -2,6 +2,7 @@ if(loadRequiredAddOn("Bot_Hole") == $Error::None && loadRequiredAddOn("Support_L
 {	
 	exec("./modules/scripts/support/support.cs");
 	exec("./modules/add-ins/player_l4b/player_l4b.cs");
+	exec("./modules/scripts/support/support_flavormessages.cs");
 	exec("./modules/add-ins/script_l4b/script_l4b.cs");
 	exec("./modules/add-ins/item_l4b/item_l4b.cs");
 	exec("./modules/add-ins/weapon_l4b/weapon_l4b.cs");
